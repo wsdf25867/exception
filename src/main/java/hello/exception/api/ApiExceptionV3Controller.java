5cec661844b3379091c60272c029ad15ec6dba38
Ryan <wsdf25867@gamail.com>
2022-05-02 22:55:57 +0900

    MVC 2편 API예외처리 끝

--- src/main/java/hello/exception/api/ApiExceptionV3Controller.java
+++ src/main/java/hello/exception/api/ApiExceptionV3Controller.java
@@ -1,23 +1,21 @@
 package hello.exception.api;
 
-import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
-public class ApiExceptionController {
+public class ApiExceptionV3Controller {
 
-    @GetMapping("/api/members/{id}")
-    public MemberDto getMember(@PathVariable("id") String id){
+
+
+    @GetMapping("/api3/members/{id}")
+    public MemberDto getMember(@PathVariable("id") String id) {
 
         if (id.equals("ex")) {
             throw new RuntimeException("잘못된 사용자");
@@ -30,21 +28,6 @@ public class ApiExceptionController {
         }
 
         return new MemberDto(id, "hello " + id);
-    }
-
-    @GetMapping("/api/response-status-ex1")
-    public String responseStatusEx1() {
-        throw new BadRequestException();
-    }
-
-    @GetMapping("/api/response-status-ex2")
-    public String responseStatusEx2() {
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
-    }
-
-    @GetMapping("/api/default-handler-ex")
-    public String defaultException(@RequestParam Integer data) {
-        return "ok";
     }
 
     @Data
